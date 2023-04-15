d0ce43279e2a13ab8db4f7d4fa773ff9d2565528
mrfritzg <mrfritz@gamil.com>
2023-04-15 00:28:42 -0400

    fixing navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,25 +2,38 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <Link to="#" className="navbar-brand mb-0 h1">
-                <img className="d-inline-block align-top" src="/logo192.png" /> NAVBAR
+        
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="container">
+            <Link to="#" 
+            className="navbar-brand mb-0 h1">
+                <img 
+                className="d-inline-block align-top pagelogo" 
+                src="/logo192.png"  alt="logo.png"/>
             </Link>
-            <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" className="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-labeL="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+            <button 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            className="navbar-toggler" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav ms-auto nav-pills">
                     <li className="nav-item active">
-                        <Link to="#" className="nav-link active">Projects</Link>
+                        <Link to="#" className="nav-link">Projects</Link>
                     </li>
-                    <li className="nav-item active">
-                        <Link to="#" className="nav-link active">Contact Me</Link>
+                    <li className="nav-item">
+                        <Link to="#" className="nav-link">Contact</Link>
                     </li>
                 </ul>
             </div>
-
+            </div>
         </nav>
+        
     )
 
 }
