3ef54d2dc5f6a3e39df6646dddeacc8118873ddc
mrfritzg <mrfritz@gamil.com>
2023-04-16 02:28:33 -0400

    add hero image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <div>
       <Navbar />
+      <div className='row'>
+      <div className='col-sm-4 color-overlay d-flex justify-content-center align-items-center'>
+          <h1>Hello World!</h1>
+        </div>
+      <div className= 'col-sm-8 masthead'>     
+      </div>
+      </div>
     </div>
   );
 }
