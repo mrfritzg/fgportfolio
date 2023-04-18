dc5660bc5f51f03eba325638d58bcfbdde0eaa9b
mrfritzg <mrfritz@gamil.com>
2023-04-18 04:18:28 -0400

    added cards

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 // import logo from './logo.svg';
 // import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
@@ -7,16 +8,81 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div className= 'masthead'> 
-      {/* <img className="d-flex justify-content-center align-items-center" src="/images/image_hero.jpg" alt="" /> */}
+      <div className='masthead'> </div>
+      <div className='color-overlay text-center'>
+        <h1 className='introText'>Fritz Guerilus Jr.</h1>
+        <h2 className='introText'>Software Engineer / Web Developer</h2>
       </div>
-        <div className='color-overlay text-center'>
-          <h1 className='introText'>Fritz Guerilus Jr.</h1>
-          <h2 className='introText'>Software Engineer / Web Developer</h2>
+      <div className='container'>
+        <h1 className='text-center'>About Me</h1>
+        <div className='row g-4'>
+          <div className='col-md-3'>
+            <img className='mypic' src='./images/fritzimage2.jpeg' alt='picture of fritz' />
+          </div>
+          <div className='col-md-9'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatum dolores doloremque aspernatur impedit animi magnam ut suscipit sunt, itaque magni, quibusdam in praesentium laboriosam minima optio facilis atque? Labore deleniti magnam eum, nisi id eaque iusto voluptatem soluta culpa, maxime porro! Facere praesentium fugiat accusamus quis ab consequatur temporibus?</p>
+          </div>
+        </div>
+        <h1 className='text-center'>Projects</h1>
+        <div className='row g-3'>
+          <div className='col-md-4'>
+            <div className="card">
+            <img src='#' alt="project image" className="card-img-top repair-img"  />
+              <div className="card-body">
+                <Link to='#'>
+                  <h5 className="card-title"><strong>Project Title1</strong></h5>
+                </Link>
+                <p className="card-text">
+                  <strong>Description: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo reprehenderit ex quo maxime atque distinctio quia esse similique magnam tempore, sit iure in perferendis nihil adipisci sed repellendus. Eum magni totam nisi ab suscipit sunt, necessitatibus laboriosam id nulla provident libero accusantium quae? Nam, esse harum molestiae tempora voluptates repellat!
+                </p>
+                <p className="card-footer">
+                  <strong>PUT BUTTON: </strong>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* PROJECT2 */}
+          <div className='col-md-4'>
+            <div className="card">
+            <img src='#' alt="project image" className="card-img-top repair-img"  />
+              <div className="card-body">
+                <Link to='#'>
+                  <h5 className="card-title"><strong>Project Title2</strong></h5>
+                </Link>
+                <p className="card-text">
+                  <strong>Description: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo reprehenderit ex quo maxime atque distinctio quia esse similique magnam tempore, sit iure in perferendis nihil adipisci sed repellendus. Eum magni totam nisi ab suscipit sunt, necessitatibus laboriosam id nulla provident libero accusantium quae? Nam, esse harum molestiae tempora voluptates repellat!
+                </p>
+                <p className="card-footer">
+                  <strong>PUT BUTTON: </strong>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* PROJECT3 */}
+          <div className='col-md-4'>
+            <div className="card">
+            <img src='#' alt="project image" className="card-img-top repair-img"  />
+              <div className="card-body">
+                <Link to='#'>
+                  <h5 className="card-title"><strong>Project Title3</strong></h5>
+                </Link>
+                <p className="card-text">
+                  <strong>Description: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo reprehenderit ex quo maxime atque distinctio quia esse similique magnam tempore, sit iure in perferendis nihil adipisci sed repellendus. Eum magni totam nisi ab suscipit sunt, necessitatibus laboriosam id nulla provident libero accusantium quae? Nam, esse harum molestiae tempora voluptates repellat!
+                </p>
+                <p className="card-footer">
+                  <strong>PUT BUTTON: </strong>
+                </p>
+              </div>
+            </div>
+          </div>
+
 
         </div>
-      
       </div>
+
+    </div>
   );
 }
 
