f3ce3717168e466db8b95a204d6ff725e7a0226c
mrfritzg <mrfritz@gamil.com>
2023-04-26 02:33:59 -0400

    added thank you

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export default function Success() {
     return (
         <>
-            <h1 className="text-center m-4">Your Message was sucessfully sent!</h1>
+            <h1 className="text-center m-4">Thank you! Your Message was sucessfully sent!</h1>
             <h2>Please return to the <Link to="/">home page</Link></h2>            
         </>
     )
